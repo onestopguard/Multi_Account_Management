74956bbd92802d2696cfc4ea9dd8061e77a4880e
onestopguard <onestopguard@github.com>
2026-04-21 21:26:53 +0800

    Minimal_test_app

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,27 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
-
 fn main() {
-    env_logger::init();
-    log::info!("Starting 账号管家公司...");
-
-    tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_clipboard_manager::init())
-        .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_fs::init())
-        .setup(|app| {
-            log::info!("App setup complete");
-            
-            // Get the main window
-            if let Some(window) = app.get_webview_window("main") {
-                window.set_title("账号管家公司").ok();
-            }
-            
-            Ok(())
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    account_manager_lib::run();
 }
